976c3190bcf5fb757edd74cfd4111984366fc8ff
Dabronzo <ayrtonterceiro@gmail.com>
2022-02-02 11:43:52 +0000

    Created the gitHub repos display and fixed bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -17,8 +17,30 @@ function userInformationHTML(user) {
         </div>`;
 }
 
+function repoInformationHTML(repos){
+    //since the repos response will be an array
+    //we check if there are repos on the user account
+    if (repos.length == 0){
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+    //using the map() method to iterate on the array and put in a HTML
+    //template of list item "li"
+    var listItemsHTML = repos.map(function(repo){
+        return `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
+    });
+
+    //now to display everything since the map return an array the "join('\n')" is used to
+    // prevent to need to iterate again on this array
+    return `<div class="clearfix repo-list"><p><strong>Repo List:</strong></p><ul>${listItemsHTML.join("\n")}</ul> </div>`
+}
+
 
 function fetchGitHubInformation(event){
+    //cleaning the userData and repoData to don't display when
+    //the text is empty
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     //creating a varible to hold the user name, using Jquery
     var username = $("#gh-username").val();
     //if statement to check if somethin was typed on the input area
@@ -33,13 +55,21 @@ function fetchGitHubInformation(event){
 
     //now here is how promise in jquery can be achieved
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        //below is the get method fot the repositories
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response){
-            var userData = response;
+        //the response then is divided by 2
+        function(firstResponse, secodResponse){
+            //since that we are using more than one response
+            //the response will be stored in an array so we need to select
+            //the first element since that is the only thing in the array
+            var userData = firstResponse[0];
+            var repoData = secodResponse[0];
             //so the "#gh-user-data" will get the content provide by the fuction
             //"userInformationHtml() that takes the "userData as argument"
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
          //here is the errorRespond in case happens
         }, function(errorResponse){
             //checking if is a 404 error
@@ -49,5 +79,9 @@ function fetchGitHubInformation(event){
                 console.log(errorResponse);
                 $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
-        } );
+    } );
+
+
 }
+//this will display the default value of the inpu (octocat)
+$("document").ready(fetchGitHubInformation);
